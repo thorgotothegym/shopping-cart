fec0c49f89c1b540dbc1f43546998c7079476f9c
Aleix Vidal <al3ix.vidal@gmail.com>
2023-03-08 22:27:01 +0100

    feat: refactor

--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllProducts } from "../../services";
 
 export const Home = () => {
+  const [data, setData] = useState<any[]>([]);
+  const alldata = () => {
+    try {
+      const response = getAllProducts();
+    } catch (error) {
+      
+    }
+  };
+  useEffect(() => {}, []);
   return <>home</>;
 };
